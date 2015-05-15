de92dbe2f4fc20cd9bde6f7316be479a4c5d4e76
lisa850728g <lisa850728g@gmail.com>
2015-05-15 11:13:04 +0800

    update calculate class
    增加計算三角形，利用輸入者輸入的三個邊長，來算出此三角形的面積和周長

--- calculate.java
+++ calculate.java
@@ -14,6 +14,20 @@ public class calculate extends JFrame{
 	double circleArea;
 	double circlePerimeter;
 	
+	JTextField jtfFirstLength = new JTextField(10);
+	JTextField jtfSecondLength = new JTextField(10);
+	JTextField jtfThirdLength = new JTextField(10);
+	JTextField jtfTriangleArea = new JTextField(10);
+	JTextField jtfTrianglePerimeter = new JTextField(10);
+	JButton jbtCaculate1 = new JButton("Calculate");
+	JButton jbtReset1 = new JButton("Reset");
+	
+	int firstLength;
+	int secondLength;
+	int thirdLength;
+	double triangleArea;
+	double trianglePerimeter;
+	
 	calculate(){
 		Panel titleCircle = new Panel(new FlowLayout(FlowLayout.LEFT));
 		titleCircle.setSize(100,100);
@@ -61,14 +75,79 @@ public class calculate extends JFrame{
 				jtfCirclePerimeter.setText(String.format("%.2f" , circlePerimeter));
 			}
 		});
+		
+		Panel titleTriangle = new Panel(new FlowLayout(FlowLayout.LEFT));
+		titleTriangle.setSize(100,100);
+		titleTriangle.add(new JLabel("Triangle : "));
+		
+		Panel setFirstLength = new Panel(new FlowLayout(FlowLayout.LEADING));
+		setFirstLength.setSize(200,100);
+		setFirstLength.add(new JLabel("First Length : "));
+		setFirstLength.add(jtfFirstLength);
+		
+		Panel setSecondLength = new Panel(new FlowLayout(FlowLayout.LEADING));
+		setSecondLength.setSize(100,100);
+		setSecondLength.add(new JLabel("Second Length : "));
+		setSecondLength.add(jtfSecondLength);
+		
+		Panel setThirdLength = new Panel(new FlowLayout(FlowLayout.LEADING));
+		setThirdLength.setSize(100,100);
+		setThirdLength.add(new JLabel("Third Length : "));
+		setThirdLength.add(jtfThirdLength);
+		setThirdLength.add(jbtCaculate1);
+		setThirdLength.add(jbtReset1);
+		
+		Panel getTriangleArea = new Panel(new FlowLayout(FlowLayout.LEADING));
+		getTriangleArea.setSize(100,100);
+		getTriangleArea.add(new JLabel("The area is : "));
+		getTriangleArea.add(jtfTriangleArea);
+		
+		Panel getTrianglePerimeter = new Panel(new FlowLayout(FlowLayout.LEADING));
+		getTrianglePerimeter.setSize(100,100);
+		getTrianglePerimeter.add(new JLabel("The perimeter is : "));
+		getTrianglePerimeter.add(jtfTrianglePerimeter);
+		
+		add(titleTriangle);
+		add(setFirstLength);
+		add(setSecondLength);
+		add(setThirdLength);
+		add(getTriangleArea);
+		add(getTrianglePerimeter);
+		
+		jbtReset1.addActionListener(new ActionListener(){
+			@Override
+			public void actionPerformed(ActionEvent e){
+				jtfFirstLength.setText("");
+				jtfSecondLength.setText("");
+				jtfThirdLength.setText("");
+				jtfTriangleArea.setText("");
+				jtfTrianglePerimeter.setText("");
+			}
+		});
+		
+		jbtCaculate1.addActionListener(new ActionListener(){
+			@Override
+			public void actionPerformed(ActionEvent e){
+				firstLength = Integer.parseInt(jtfFirstLength.getText());
+				secondLength = Integer.parseInt(jtfSecondLength.getText());
+				thirdLength = Integer.parseInt(jtfThirdLength.getText());
+				trianglePerimeter = firstLength + secondLength + thirdLength;
+				triangleArea = Math.pow(((trianglePerimeter/2) * (trianglePerimeter/2 - firstLength) * 
+							(trianglePerimeter/2 - secondLength) * (trianglePerimeter/2 - thirdLength)) , 0.5);
+				
+	
+				jtfTriangleArea.setText(String.format("%.2f" , triangleArea));
+				jtfTrianglePerimeter.setText(String.format("%.2f" , trianglePerimeter));
+			}
+		});
 	}
 	public static void main(String[] args){
 		calculate frame = new calculate();
 		frame.setTitle("U10316011_GeoCalculate");
 		frame.setLocationRelativeTo(null);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	  frame.setSize(400, 480);
-	  frame.setVisible(true);
-		frame.setLayout(new GridLayout(4,1));
+		frame.setSize(400, 480);
+		frame.setVisible(true);
+		frame.setLayout(new GridLayout(10,1));
 	}
 }
